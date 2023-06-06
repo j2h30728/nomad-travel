a380047bdec0588c55ea741245bf518dd23b3ada
LeeJiHyeon <rachel2148072@gmail.com>
2023-06-06 22:58:07 +0900

    feat: recoil persist 적용으로 travelsState 로컬스토리지 저장

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,6 +1,9 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
 
 import { TravelType, CategoriesType } from "../types/travel";
+
+const { persistAtom } = recoilPersist();
 
 export const travelState = atom<TravelType>({
   key: "travelState",
@@ -14,4 +17,5 @@ export const travelState = atom<TravelType>({
 export const travelsState = atom<Array<TravelType>>({
   key: "travelsState",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
