2ad843193a36954ecb81643d93ed75c3e29fd581
LeeJiHyeon <rachel2148072@gmail.com>
2023-06-06 23:46:37 +0900

    fix: 잘못된 타이틀명 변경

--- src/components/LikeCountries.tsx
+++ src/components/LikeCountries.tsx
@@ -14,7 +14,7 @@ const LikeCountries = () => {
 
   return (
     <>
-      <h2>내가 가본 나라들</h2>
+      <h2>내가 좋아하는 나라들</h2>
       {favoriteTravles.map(travel => {
         return (
           <div key={travel.travelId}>
